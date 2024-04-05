f49d142af2f4346deb722a0b01f0cbec2127e4ec
gustavo <gustavo.gama066@academico.ifs.edu.br>
2024-04-05 15:11:18 -0300

    Inicio do desenvolvimento

---
+++ src/test/java/ifs/edu/br/portal/PortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ifs.edu.br.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
