55a7f4ddc46b51ebe5ffb083ee5fbb80ca9c1eab
gustavo <gustavo.gama066@academico.ifs.edu.br>
2024-04-09 15:08:09 -0300

    feat: Adição de metodos de buscas por filtro

--- src/main/java/ifs/edu/br/portal/Repository/MapaRepository.java
+++ src/main/java/ifs/edu/br/portal/Repository/MapaRepository.java
@@ -1,10 +1,18 @@
 package ifs.edu.br.portal.Repository;
 
+import ifs.edu.br.portal.entity.Categoria;
 import ifs.edu.br.portal.entity.Mapa;
+import ifs.edu.br.portal.entity.PontoTempo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MapaRepository extends JpaRepository<Mapa, Integer> {
+
+
+    List<Mapa> findAllByIdOrNomeOrCategoriasOrPontoTemposOrderByNome(
+            Integer id, String nome, List<Categoria> categorias, List<PontoTempo> pontoTempos);
+
 }
