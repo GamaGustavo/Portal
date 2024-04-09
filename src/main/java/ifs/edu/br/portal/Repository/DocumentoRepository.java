f5480b04b8ac4b29120797479ff6fad6852d9177
gustavo <gustavo.gama066@academico.ifs.edu.br>
2024-04-09 15:07:26 -0300

    feat: Adição de metodos de buscas por pontoTempo

--- src/main/java/ifs/edu/br/portal/Repository/DocumentoRepository.java
+++ src/main/java/ifs/edu/br/portal/Repository/DocumentoRepository.java
@@ -2,8 +2,15 @@ package ifs.edu.br.portal.Repository;
 
 import ifs.edu.br.portal.entity.Documento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DocumentoRepository extends JpaRepository<Documento, Integer> {
+    @Query(nativeQuery = true, value = "select doc.* from documento doc " +
+            "  join ponto_tempo_documentos ptdoc on ptdoc.documentos_id = doc.id " +
+            "  join ponto_tempo pt on ptdoc.ponto_tempo_id = pt.id where pt.id = ?1")
+    List<Documento> findAllByPontoTempo(Integer id);
 }
--- src/main/java/ifs/edu/br/portal/Repository/ShapeFileRepository.java
+++ src/main/java/ifs/edu/br/portal/Repository/ShapeFileRepository.java
@@ -2,8 +2,15 @@ package ifs.edu.br.portal.Repository;
 
 import ifs.edu.br.portal.entity.ShapeFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ShapeFileRepository extends JpaRepository<ShapeFile, Integer> {
+    @Query(nativeQuery = true, value = "select sf.* from shape_file sf " +
+            "  join ponto_tempo_shape_files ptsf on ptsf.shape_files_id = sf.id " +
+            "  join ponto_tempo pt on ptsf.ponto_tempo_id = pt.id where pt.id = ?1")
+    List<ShapeFile> findAllByPontoTempo(Integer id);
 }
