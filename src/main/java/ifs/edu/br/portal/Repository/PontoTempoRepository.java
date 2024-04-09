f0649ac7ad8270e092774aaa1ad5a153c415a52b
gustavo <gustavo.gama066@academico.ifs.edu.br>
2024-04-09 15:08:30 -0300

    feat: Adição de metodos de buscas por mapa

--- src/main/java/ifs/edu/br/portal/Repository/PontoTempoRepository.java
+++ src/main/java/ifs/edu/br/portal/Repository/PontoTempoRepository.java
@@ -1,9 +1,17 @@
 package ifs.edu.br.portal.Repository;
 
+import ifs.edu.br.portal.entity.Documento;
 import ifs.edu.br.portal.entity.PontoTempo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PontoTempoRepository extends JpaRepository<PontoTempo, Integer> {
+    @Query(nativeQuery = true, value = "select pt.* from ponto_tempo pt "+
+            "  join mapa_ponto_tempos mpt on mpt.ponto_tempos_id = pt.id " +
+            "  join mapa map on map.id = mpt.mapa_id where map.id = ?1")
+    List<PontoTempo> findAllByMapa(Integer idMapa);
 }
