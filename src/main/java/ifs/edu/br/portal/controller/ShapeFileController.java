81c130c5106653bfa938d2f7dafc2d9d9834b774
gustavo <gustavo98ag@gmail.com>
2024-06-11 21:56:11 -0300

    Feat: Adição dos metodos de request do ShapeFile

--- src/main/java/ifs/edu/br/portal/controller/ShapeFileController.java
+++ src/main/java/ifs/edu/br/portal/controller/ShapeFileController.java
@@ -1,23 +1,45 @@
 package ifs.edu.br.portal.controller;
 
+import ifs.edu.br.portal.entity.ShapeFile;
 import ifs.edu.br.portal.service.ShapeFileService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class ShapeFileController {
-    @Autowired
-  private  ShapeFileService shapeFileService;
+
+    private final ShapeFileService shapeFileService;
+
+    public ShapeFileController(ShapeFileService shapeFileService) {
+        this.shapeFileService = shapeFileService;
+    }
 
 
     @PostMapping("/uploadShapefile")
-    public String uploadShapefile(@RequestParam("file") MultipartFile file) {
-        if (file.isEmpty()) {
-            return "O arquivo está vazio";
+    public String uploadShapefile(@RequestBody MultipartFile file)  {
+        if (file != null && !file.isEmpty()) {
+            return shapeFileService.uploadShapefile(file);
         }
-       return shapeFileService.uploadShapefile(file);
+        return "O arquivo está vazio";
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> cadastrar(@RequestBody ShapeFile shapeFile) {
+        return ResponseEntity.ok(shapeFileService.cadastrar(shapeFile));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<ShapeFile> deletar(@RequestParam Integer id) {
+        ShapeFile shapeFile = shapeFileService.deletar(id).orElseThrow();
+        return ResponseEntity.ok(shapeFile);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ShapeFile>> listar(@RequestParam Integer pontTempo) {
+        return ResponseEntity.ok(shapeFileService.listarPorPonto(pontTempo));
     }
 }
--- src/main/java/ifs/edu/br/portal/service/ShapeFileService.java
+++ src/main/java/ifs/edu/br/portal/service/ShapeFileService.java
@@ -1,30 +1,35 @@
 package ifs.edu.br.portal.service;
-import ifs.edu.br.portal.repository.ShapeFileRepository;
+
 import ifs.edu.br.portal.entity.ShapeFile;
+import ifs.edu.br.portal.repository.ShapeFileRepository;
 import org.geotools.api.data.DataStore;
+import org.geotools.api.data.DataStoreFinder;
+import org.geotools.api.data.SimpleFeatureSource;
 import org.geotools.api.data.SimpleFeatureStore;
-import org.geotools.api.data.Transaction;
-import org.geotools.api.feature.simple.SimpleFeature;
 import org.geotools.api.feature.simple.SimpleFeatureType;
-import org.geotools.data.shapefile.ShapefileDataStore;
-import org.geotools.data.shapefile.ShapefileDataStoreFactory;
-import org.geotools.feature.FeatureCollection;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.geotools.api.filter.Filter;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class ShapeFileService {
-    @Autowired
-    private ShapeFileRepository repository;
-    @Autowired
-    private DataStore dataStore;
+    private final ShapeFileRepository repository;
+
+    private final DataStore dataStore;
+
+    public ShapeFileService(ShapeFileRepository repository, DataStore dataStore) {
+        this.repository = repository;
+        this.dataStore = dataStore;
+    }
 
     @Transactional
     public Integer cadastrar(ShapeFile obj) {
@@ -43,25 +48,49 @@ public class ShapeFileService {
         return repository.findAllByPontoTempo(idPonto);
     }
 
+   /* public String uploadShapefile(MultipartFile file) throws IOException {
+        try (InputStream inputStream = file.getInputStream()) {
+            byte[] magicNumberBytes = new byte[4];
+            if (inputStream.read(magicNumberBytes) != 4) {
+                Logger.getLogger(this.getClass().getName()).severe("False");
+                return "false";
+            }
+            int magicNumber = ByteBuffer.wrap(magicNumberBytes).order(ByteOrder.BIG_ENDIAN).getInt();
+            Logger.getLogger(this.getClass().getName()).severe(magicNumber + "");
+            return "" + (magicNumber == 9994);
 
-    public String uploadShapefile(MultipartFile file){
+        }*/
+
+    public String uploadShapefile(MultipartFile file) {
         try {
-            // Salva o arquivo no disco temporariamente
-            Path tempFile = Files.createTempFile("shapefile", ".shp");
-            Files.write(tempFile, file.getBytes());
+            Path tempFile = getTempFile(file);
 
             // Cria o ShapefileDataStore
-            ShapefileDataStoreFactory dataStoreFactory = new ShapefileDataStoreFactory();
+            DataStore dataTempFile = DataStoreFinder.getDataStore(Collections.singletonMap("url", tempFile.toUri().toURL()));
+
+            // Lê as features do shapefile
+            String inputTypeName = Arrays.stream(dataTempFile.getTypeNames()).findFirst().orElse("");
+            SimpleFeatureType simpleFeatureType = dataTempFile.getSchema(inputTypeName);
+            SimpleFeatureSource source = dataTempFile.getFeatureSource(inputTypeName);
+
+            // Adiciona as features no DataStore (PostGIS)
+            dataStore.createSchema(simpleFeatureType);
+            SimpleFeatureStore featureStore = (SimpleFeatureStore) dataStore.getFeatureSource(inputTypeName);
+            featureStore.addFeatures(source.getFeatures(Filter.INCLUDE));
+            dataTempFile.dispose();
+            /*
+
+           ShapefileDataStoreFactory dataStoreFactory = new ShapefileDataStoreFactory();
             ShapefileDataStore shapefileDataStore = (ShapefileDataStore) dataStoreFactory.createDataStore(tempFile.toUri().toURL());
 
             // Lê as features do shapefile
             FeatureCollection<SimpleFeatureType, SimpleFeature> featureCollection = shapefileDataStore.getFeatureSource().getFeatures();
 
             // Adiciona as features no DataStore (PostGIS)
-            SimpleFeatureStore featureStore = (SimpleFeatureStore) dataStore.getFeatureSource(file.getName());
+           SimpleFeatureStore featureStore = (SimpleFeatureStore) dataStore.getFeatureSource(file.getName());
             featureStore.setTransaction(Transaction.AUTO_COMMIT);
             featureStore.addFeatures(featureCollection);
-
+*/
             // Deleta o arquivo temporário
             Files.delete(tempFile);
 
@@ -71,4 +100,13 @@ public class ShapeFileService {
             return "Erro ao carregar o shapefile: " + e.getMessage();
         }
     }
+
+    private static Path getTempFile(MultipartFile file) throws IOException {
+        // Salva o arquivo no disco temporariamente
+        Path tempFile = Files.createTempFile(file.getOriginalFilename().replace(".shp",""), ".shp");
+        Files.write(tempFile, file.getBytes());
+        return tempFile;
+    }
+
 }
+
