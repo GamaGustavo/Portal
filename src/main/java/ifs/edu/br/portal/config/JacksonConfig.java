4ce83a0c8b4511bbd7d978025743d05b99fa0cd2
gustavo <gustavo98ag@gmail.com>
2024-08-29 14:41:15 -0300

    feat: Adição de configuração do json.
    A modificação atribui null as propiedades que não estão presentes no json

---
+++ src/main/java/ifs/edu/br/portal/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package ifs.edu.br.portal.config;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+        return mapper;
+    }
+}
