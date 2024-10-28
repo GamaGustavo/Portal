aa6e281f39ec7ab309cad3308cac0b8a56bdbb91
gustavo <gustavo98ag@gmail.com>
2024-10-28 09:31:03 -0300

    Feat: Adição de configuração de acesso

---
+++ src/main/java/ifs/edu/br/portal/config/WebConfig.java
@@ -0,0 +1,17 @@
+package ifs.edu.br.portal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE");
+        }
+    }
+
+
